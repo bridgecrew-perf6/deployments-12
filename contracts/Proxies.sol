1ff515676078295e297222d9146f8475495543f5
Nick <103326418+anarchycapital@users.noreply.github.com>
2022-05-23 15:11:35 -0400

    abi

---
+++ contracts/Proxies.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
+
+import "@akxlab/base/Proxies/ERC721LazyMintTransferProxy.sol";
+import "@akxlab/base/Proxies/TransferProxy.sol";
+import "@akxlab/base/Proxies/ERC20TransferProxy.sol";
